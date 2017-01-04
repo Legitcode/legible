3ac5e70fb6eb73089f7dabd421b489a45e505890
Ray Gesualdo <raygesualdo@users.noreply.github.com>
2017-01-04 13:32:29 -0500

    Abstract out fetch (#20)
    * pulled fetch out from partial and request
    
    * added tests for fetch.js

--- src/request.js
+++ src/request.js
@@ -1,13 +1,8 @@
-import fetch from 'isomorphic-fetch'
+import fetch from './utilities/fetch'
 import normalize from './utilities/normalize'
 
 export default (strings, ...vars) => {
   let { options, url } = normalize(strings, vars)
 
-  return new Promise((resolve, reject) => {
-    fetch(url, options)
-    .then(response => response.json())
-    .then(json => resolve(json))
-    .catch(error => reject(error))
-  })
+  return fetch(url, options)
 }
