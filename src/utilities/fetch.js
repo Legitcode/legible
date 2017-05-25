bc4483c7a321ff0b08a2ace28eb5211e2fbb01be
Zach Silveira <zackify@gmail.com>
2017-05-25 16:31:04 -0400

    Fix uncaught fetch

--- src/utilities/fetch.js
+++ src/utilities/fetch.js
@@ -1,22 +1,26 @@
-import fetch from 'isomorphic-fetch'
+import fetch from 'isomorphic-fetch';
 
 export default (url, options = {}) => {
   return new Promise((resolve, reject) => {
     fetch(url, options)
-    .then(response => {
-      if (options.onResponse && options.onResponse(response)) return false
+      .then(response => {
+        if (options.onResponse && options.onResponse(response)) return false;
 
-      response.text()
-      .then(text => {
-        try {
-          resolve(JSON.parse(text))
-        } catch (e) {
-          resolve({})
-        }
+        response
+          .text()
+          .then(text => {
+            try {
+              resolve(JSON.parse(text));
+            } catch (e) {
+              resolve({});
+            }
+          })
+          .catch(function(error) {
+            return reject(error);
+          });
       })
-      .catch(function (error) {
-        return reject(error)
-      })
-    })
-  })
-}
+      .catch(function(error) {
+        return reject(error);
+      });
+  });
+};
--- tests/unit/request.js
+++ tests/unit/request.js
@@ -1,19 +1,19 @@
-import request from '../../src'
+import request from '../../src';
 
 describe('request', () => {
-  it('returns value from response', async function () {
+  it('returns value from response', async function() {
     let response = await request`
       url: https://freegeoip.net/json/github.com
-    `
-    expect(response.country_code).to.equal('US')
-  })
+    `;
+    expect(response.country_code).to.equal('US');
+  });
 
-  it('returns headers on response', async function () {
+  it('returns headers on response', async function() {
     await request`
       url: https://freegeoip.net/json/github.com
       onResponse: ${response => {
-        expect(response.headers.get('content-type')).to.equal('application/json')
-      }}
-    `
-  })
-})
+      expect(response.headers.get('content-type')).to.equal('application/json');
+    }}
+    `;
+  });
+});
